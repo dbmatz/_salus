2c403ee51790051698a41671954afc4e11440e70
dbmatz <matzenbacherdebora@gmail.com>
2023-11-04 09:03:46 -0400

    Adicionando insert automatico em tabela emocoes

--- database/migrations/2023_09_03_193713_create_emocaos_table.php
+++ database/migrations/2023_09_03_193713_create_emocaos_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -16,9 +15,18 @@ return new class extends Migration
             $table->string('nome')->unique();
             $table->string('imagem');
         });
+
+        DB::table('emocaos')->insert([['id' => 1, 'nome' => 'Feliz', 'imagem' => '/feliz.jpg'],
+        ['id' => 2, 'nome' => 'Triste', 'imagem' => '/triste.jpg'],
+        ['id' => 3, 'nome' => 'Surpreso', 'imagem' => '/surpreso.jpg'],
+        ['id' => 4, 'nome' => 'Bravo', 'imagem' => '/bravo.jpg'],
+        ['id' => 5, 'nome' => 'Neutro', 'imagem' => '/neutro.jpg'],
+        ['id' => 6, 'nome' => 'Sonhador', 'imagem' => '/sonhador.jpg']]
+    );
     }
 
     /**
+     * 
      * Reverse the migrations.
      */
     public function down(): void
